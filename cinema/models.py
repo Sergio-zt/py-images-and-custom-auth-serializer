c9e0ad567967b68d6ecfef267e5aa3010378c9d2
Sergio-zt <fop.gso@gmail.com>
2026-08-08 14:39:17 -0400

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -44,7 +44,7 @@ def movie_image_path(instance: "Movie", filename: str) -> str:
         f"{slugify(instance.title)}-{uuid.uuid4()}"
         + pathlib.Path(filename).suffix
     )
-    return pathlib.Path("uploads/movies/") / pathlib.Path(filename)
+    return str(pathlib.Path("uploads/movies/") / pathlib.Path(filename))
 
 
 class Movie(models.Model):
--- cinema/serializers.py
+++ cinema/serializers.py
@@ -31,6 +31,7 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 
 
 class MovieImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField()
     class Meta:
         model = Movie
         fields = ("id", "image")
@@ -92,7 +93,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
         source="cinema_hall.capacity", read_only=True
     )
     tickets_available = serializers.IntegerField(read_only=True)
-    movie_image = serializers.CharField(source="movie.image", read_only=True)
+    movie_image = serializers.ImageField(source="movie.image", read_only=True)
 
     class Meta:
         model = MovieSession
