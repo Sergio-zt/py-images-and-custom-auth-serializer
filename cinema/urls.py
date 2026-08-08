a3cdbc1af9fbb05c41e5189e20feeb1ffd18af93
Sergio-zt <fop.gso@gmail.com>
2026-08-08 14:15:17 -0400

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 
 from cinema.views import (
@@ -18,6 +20,8 @@ router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 app_name = "cinema"
